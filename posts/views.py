32fa24856cceac6d5784a85edac9226fafcc0ba9
AngelMValentin <angelvalentinusmc@gmail.com>
2025-02-19 19:05:17 -0800

    first commit

---
+++ posts/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import (
+    UpdateView,
+    DeleteView
+)
+from .models import Post
+from django.urls import reverse_lazy
+
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = [
+        "title", "subtitle", "body",
+        "author",
+    ]
+
+    class PostDeleteView(DeleteView):
+        template_name = "posts/delete.html"
+        model = Post
+        success_url = reverse_lazy("list") #we dont have url patterns yet so list isn't a url pattern yet
+
+# Create your views here.
